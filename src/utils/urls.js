01b9a29f6a3321c214ca6f1808888fe719ac7a8c
malvinamaria <mmpolak@protonmail.com>
2023-06-14 11:10:15 +0200

    fix fix fix

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://webshop-backend-4yp4ksq4oa-no.a.run.app/';
+const BASE_URL = 'https://webshop-backend-4yp4ksq4oa-no.a.run.app';
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
